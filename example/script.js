e3add1dfb787885ac374291461299e304176782f
Glitch (chiseled-shape) <none>
2019-05-17 13:51:00 +0000

    🐢👵 Updated with Glitch

---
+++ example/script.js
@@ -0,0 +1,102 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+/*
+// prints "hi" in the browser's dev tools console
+console.log('hi');
+
+let x = 2, y = 3, z=6;
+console.log("Sum:", x, "+", y, "=", x+y);
+console.log("Difference:", z, "-", y, "=", z-y);
+console.log("Product:", y, "*", z, "=", y*z);
+console.log("Quotient 1:", y, "/", z, "=", y/z);
+console.log("Quotient 2:", z, "/", x, "=", z/x);
+console.log("Remainder 1:", z, "%", x, "=", z%x);
+console.log("Remainder 2:", x, "%", z, "=", x%z);
+console.log("Divide into zero:", 0, "/", x, "=", 0/x);
+console.log("Divide by zero:", x, "/", 0, "=", x/0);
+console.log("Zero into zero:", 0, "/", 0, "=", 0/0);
+console.log("Remainder and zero 1:", 0, "%", x, "=", 0%x);
+console.log("Remainder and zero 2:", x, "%", 0, "=", x%0);
+console.log("Remainder and zero 3:", 0, "%", 0, "=", 0%0);
+console.log("Power 1:", x, "**", y, "=", x**y);
+console.log("Power 2:", x, "**", -y, "=", x**-y);
+*/
+/*
+let x = 1;
+let y = 2;
+let z = 3;
+
+if (x > y) {
+  console.log(x+" is greater than "+y);
+}
+
+let vowels = "aeiou";
+
+let sentence = prompt("Enter a sentence");
+let letter = prompt("Enter a letter");
+
+if (letter in vowels) 
+  console.log("Vowel");
+*/
+
+// A number of parsons problems
+
+/*
+// PROGRAM 1
+// declare a variable (undefined)
+// ask to enter 3 values
+let studentResult;
+for (let counter = 0; counter < 3; counter++) {
+  studentResult = Number(prompt("Enter result"));
+  if (studentResult >= 90) {
+    console.log("H1 or O1");
+  } 
+} // end for loop
+*/
+
+/*
+// PROGRAM 2 - same as above with a while loop
+let studentResult;
+let counter = 0;
+while (counter < 3) {
+  studentResult = Number(prompt("Enter result"));
+  if (studentResult >= 90) {
+    console.log("H1 or O1");
+  }
+  counter++;
+} // end while loop
+*/
+
+
+
+
+/*
+let studentResult;
+for (let counter = 0; counter < 3; counter++) {
+  studentResult = Number(prompt("Enter result"));
+  if (studentResult >= 90) {
+    console.log("Grade for student "+counter+" H1 or O1");    
+  }
+} // end for loop
+*/
+
+/*
+let country = prompt("Enter a country and I will tell you its capital");
+
+if (country == "Ireland") {
+   console.log("Dublin");
+} else if (country == "Scotland") {
+   console.log("Edinburgh");
+} else if (country == "England") {
+   console.log("London");
+} else if (country == "Wales") {
+   console.log("Cardif");
+} else if (country == "Ireland") {
+   console.log("Dublin");
+
+} 
+else {
+  console.log("Unknown country. Sorry.");
+}
+
+*/
